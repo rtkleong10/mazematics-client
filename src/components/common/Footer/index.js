569a1c9d35a45a0fa89d29d1350280d1ed09f64e
rtkleong10 <rtkleong10@gmail.com>
2020-03-19 23:19:46 +0800

    [update] Added simple snapshot tests

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-const Footer = () => (
-    <footer className="container py-4 mt-4 text-muted text-right">
-        Made with <FontAwesomeIcon icon={faHeart} className="text-danger" /> by ItWorksOnMyMachine
-    </footer>
-);
-
-export default Footer;
+export default function Footer() {
+    return (
+        <footer className="container py-4 mt-4 text-muted text-right">
+            Made with <FontAwesomeIcon icon={faHeart} className="text-danger" /> by ItWorksOnMyMachine
+        </footer>
+    );
+}
