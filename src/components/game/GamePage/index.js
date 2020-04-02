248f38945eaf37fd7521c3eda46749a836902d3e
rtkleong10 <rtkleong10@gmail.com>
2020-04-02 14:17:39 +0800

    [update] Cleaned up Game system file names, indentation and test files

--- src/components/game/GamePage/index.js
+++ src/components/game/GamePage/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import World from "../world";
+import World from "../World";
 // import ReactPlayer from "react-player";
 // import Timer from "../src/components/timer/"
 /**
@@ -9,15 +9,15 @@ export class GamePage extends Component {
     render() {
         return (
             <div>
-            <World />
-            {/* <Timer/> */}
-            {/* <ReactPlayer
+                <World />
+                {/* <Timer/> */}
+                {/* <ReactPlayer
               width="0%"
               height="0%"
               url="https://www.youtube.com/watch?v=5O3a5opHbY4&t=18s"
               playing
             /> */}
-          </div>
+            </div>
         )
     }
 }
--- src/components/game/world/index.js
+++ src/components/game/world/index.js
@@ -1,83 +1,83 @@
 import React, { Component } from "react";
-import Player from "../player";
-import Map from "../map";
+import Player from "../Player";
+import Map from "../Map";
 import { tiles } from "../../../utils/data/maps/1";
 import { SPRITE_SIZE } from "../../../utils/constants";
-import Popup from "../popup/popup";
+import Popup from "../Popup";
 /**
  * This component displays the world in the game for the user. It renders the player and map.
  */
 class World extends Component {
-  constructor(props) {
-    super(props);
-    // this.handleKeyPress = this.handleKeyPress.bind(this);
-    this.state = {
-      tiles: tiles,
-      showPopup: false
-      // questions: 
+    constructor(props) {
+        super(props);
+        // this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.state = {
+            tiles: tiles,
+            showPopup: false
+            // questions: 
+        }
     }
-  }
 
-  componentDidMount() {
-    window.addEventListener("keydown", e => {
-      this.handleKeyDown(e);
-    });
-  }
-
-  hidePopup = () => {
-    this.setState({
-      showPopup: false
-    });
-  }
-
-  handleKeyDown(e) {
-    e.preventDefault();
-
-    switch (e.keyCode) {
-      case 27:
-        this.hidePopup();
-        //console.log("SHOWPOPUP: ", this.state.showPopup);
-        return;
-      default:
-        console.log(e.keyCode);
+    componentDidMount() {
+        window.addEventListener("keydown", e => {
+            this.handleKeyDown(e);
+        });
     }
-  }
 
-  handleRemoveObstacle = (pos) => {
-    var newTiles = this.state.tiles;
-    const y = pos[1] / SPRITE_SIZE; //40 divide 40 = 1 step
-    const x = pos[0] / SPRITE_SIZE;
-    newTiles[y][x] = 0;
+    hidePopup = () => {
+        this.setState({
+            showPopup: false
+        });
+    }
 
-    this.setState({
-      tiles: newTiles,
-      showPopup: true
-    });
-  }
+    handleKeyDown(e) {
+        e.preventDefault();
 
-  render() {
-    return (
-      <div
-        style={{
-          position: "relative",
-          width: "1200px",
-          height: "640px",
-          margin: "20px auto"
-        }}
-      >
-        <Map tiles={this.state.tiles} />
-        <Player tiles={this.state.tiles} showPopup={this.state.showPopup} handleRemoveObstacle={this.handleRemoveObstacle} />
+        switch (e.keyCode) {
+            case 27:
+                this.hidePopup();
+                //console.log("SHOWPOPUP: ", this.state.showPopup);
+                return;
+            default:
+                console.log(e.keyCode);
+        }
+    }
 
-        {this.state.showPopup ? (
-          <Popup
-            //TODO: fetches question according to coordinates
-            text="QUIZ QUESTION"
-            closePopup={this.hidePopup.bind(this)}
-          />
-        ) : null}
-      </div>
-    );
-  }
+    handleRemoveObstacle = (pos) => {
+        var newTiles = this.state.tiles;
+        const y = pos[1] / SPRITE_SIZE; //40 divide 40 = 1 step
+        const x = pos[0] / SPRITE_SIZE;
+        newTiles[y][x] = 0;
+
+        this.setState({
+            tiles: newTiles,
+            showPopup: true
+        });
+    }
+
+    render() {
+        return (
+            <div
+                style={{
+                    position: "relative",
+                    width: "1200px",
+                    height: "640px",
+                    margin: "20px auto"
+                }}
+            >
+                <Map tiles={this.state.tiles} />
+                <Player tiles={this.state.tiles} showPopup={this.state.showPopup} handleRemoveObstacle={this.handleRemoveObstacle} />
+
+                {this.state.showPopup ? (
+                    <Popup
+                        //TODO: fetches question according to coordinates
+                        text="QUIZ QUESTION"
+                        closePopup={this.hidePopup.bind(this)}
+                    />
+                ) : null}
+            </div>
+        );
+    }
 }
 
 export default World;
