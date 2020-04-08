83f94c3eb8b587f8c927e422c0ee775257206a12
rtkleong10 <rtkleong10@gmail.com>
2020-04-08 15:20:37 +0800

    [update] Added QuestionModal and QuestionForm to the game

--- src/components/game/World/index.js
+++ src/components/game/World/index.js
@@ -1,31 +1,29 @@
 import React, { Component } from "react";
+
 import Player from "../Player";
 import Map from "../Map";
 import { SPRITE_SIZE, MAP_WIDTH, MAP_HEIGHT } from "../../../utils/constants";
-import Popup from "../Popup";
+import QuestionModal from "../QuestionModal";
 
 /**
  * This component displays the world in the game for the user. It renders the player and map.
  */
 class World extends Component {
-    constructor(props) {
-        super(props);
-        // this.handleKeyPress = this.handleKeyPress.bind(this);
-        this.state = {
-            tiles: props.tiles,
-            showPopup: false
-            // questions: 
-        }
+    state = {
+        tiles: this.props.tiles,
+        questionModal: {
+            isVisible: false,
+            question: {
+                id: null,
+                questionText: '',
+                options: [],
+            },
+            pos: null,
+        },
     }
 
     componentDidMount() {
-        this.worldRef.focus();
-    }
-
-    hidePopup = () => {
-        this.setState({
-            showPopup: false
-        });
+        this.mapRef.focus();
     }
 
     handleKeyDown = e => {
@@ -42,19 +40,53 @@ class World extends Component {
         }
     }
 
-    handleRemoveObstacle = (pos) => {
-        var newTiles = this.state.tiles;
+    handleEncounterObstacle = (pos) => {
         const y = pos[1] / SPRITE_SIZE; //40 divide 40 = 1 step
         const x = pos[0] / SPRITE_SIZE;
-        newTiles[y][x] = 0;
+
+        const question = this.props.questions.find(question => question.coordinates.x === x && question.coordinates.y === y);
 
         this.setState({
-            tiles: newTiles,
-            showPopup: true
+            questionModal: {
+                isVisible: true,
+                question: question,
+                pos: pos,
+            },
         });
     }
 
+    handleQuestionModalClose = correctAnswer => {
+        this.mapRef.focus();
+
+        this.setState({
+            questionModal: {
+                ...this.state.questionModal,
+                isVisible: false,
+            }
+        });
+
+        if (correctAnswer) {
+            const pos = this.state.questionModal.pos;
+            const y = pos[1] / SPRITE_SIZE; //40 divide 40 = 1 step
+            const x = pos[0] / SPRITE_SIZE;
+            var newTiles = this.state.tiles;
+            newTiles[y][x] = 0;
+
+            this.setState({
+                tiles: newTiles,
+            });
+        }
+    }
+
     render() {
+        const {
+            levelId,
+        } = this.props;
+
+        const {
+            questionModal,
+        } = this.state;
+
         return (
             <div
                 style={{
@@ -62,22 +94,16 @@ class World extends Component {
                     width: `${MAP_WIDTH * 40}px`,
                     height: `${MAP_HEIGHT * 40}px`,
                     margin: "20px auto",
-                    outline: 0,
                 }}
-                tabIndex="0"
-                onKeyDown={this.handleKeyDown}
-                ref={worldRef => this.worldRef = worldRef}
                 >
-                <Map tiles={this.state.tiles} />
-                <Player ref={playerRef => this.playerRef = playerRef} tiles={this.state.tiles} showPopup={this.state.showPopup} handleRemoveObstacle={this.handleRemoveObstacle} />
-
-                {this.state.showPopup ? (
-                    <Popup
-                        //TODO: fetches question according to coordinates
-                        text="QUIZ QUESTION"
-                        closePopup={this.hidePopup.bind(this)}
+                <Map tiles={this.state.tiles} onKeyDown={this.handleKeyDown} ref={mapRef => this.mapRef = mapRef} />
+                <Player ref={playerRef => this.playerRef = playerRef} tiles={this.state.tiles} showPopup={this.state.showPopup} handleEncounterObstacle={this.handleEncounterObstacle} />
+                <QuestionModal
+                    question={questionModal.question}
+                    isVisible={questionModal.isVisible}
+                    onClose={this.handleQuestionModalClose}
+                    levelId={levelId}
                     />
-                ) : null}
             </div>
         );
     }
