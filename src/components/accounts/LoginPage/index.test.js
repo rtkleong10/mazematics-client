5a0d01270e99320906823eb0a3eb3b05e3f6ea7a
nanyoci <over_nan@hotmail.com>
2020-04-15 11:58:17 +0800

    add antoher test case

--- src/components/accounts/LoginPage/index.test.js
+++ src/components/accounts/LoginPage/index.test.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { render, cleanup, fireEvent, screen } from '@testing-library/react'
 import LoginPage from './index.js';
 import { renderWithReduxRouter } from '../../../utils/tests.js';
-import { LOGIN, loginAction } from '../../../redux/ducks/auth';
-import axiosMock from 'axios'
+
+describe('Unit Testing for Login Page', () => {
 
 afterEach(() => {
     cleanup();
 });
 
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 it('should take a snapshot', () => {
     const { asFragment } = renderWithReduxRouter(<LoginPage />);
@@ -30,11 +31,27 @@ it('allows the user to login successfully', async () => {
     target: {value: 'admin123'},
   })
   fireEvent.click(screen.getByTestId("loginButton"))
-
-
-  const delay = ms => new Promise(res => setTimeout(res, ms));
-  await delay(5000)
+  await delay(1000)
   expect(window.localStorage.getItem('access_token')).toEqual(fakeUserResponse.access_token)
+  window.localStorage.removeItem('access_token')
 })
 
+it('wrong password reject user login ', async () => {
+  jest.spyOn(window, 'fetch').mockImplementationOnce(() => {
+    return Promise.resolve({
+      json: () => Promise.reject(),
+    })
+  })
+  renderWithReduxRouter(<LoginPage />)
+  fireEvent.change(screen.getByTestId("usernameField").querySelector('input'), {
+    target: {value: 'admin1@test.com'},
+  })
+  fireEvent.change(screen.getByTestId("passwordField").querySelector('input'), {
+    target: {value: 'wrong password'},
+  })
+  fireEvent.click(screen.getByTestId("loginButton"))
+  await delay(1000)
+  expect(window.localStorage.getItem('access_token')).toEqual(null)
+})
 
+})
